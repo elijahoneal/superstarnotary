78f7a2ac6fe5cc815c12dc44c8b0ccb2a0f1ebcb
Elijah <elijahoneal@yahoo.com>
2021-07-10 16:40:20 -0400

    Install Packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Nav
       </header>
+      <main>
+        <article>
+          <h2>About</h2>
+          <p>MOBILE NOTARY SERVICES Offering General Notary Public services, specializing in Mortgage Loan Closings. 
+            **NNA Certified, YEARLY background checked, INSURED & BONDED** Providing unrushed, quality customer service. 
+            Can meet at residential, business, jail, nursing home or hospital for document signing. Call for scheduling!</p>
+        </article>
+        <section>
+          <h2>Services</h2>
+          <section>
+            <article>
+                <div>
+                  <h4>Loan Signing Services</h4>
+                  <p>From $125.00</p>
+                </div>
+                <p>Home loan packages- can perform Purchase/Seller files, VA/FHA, Refinance, HELOC. Will print loan documents and meet at your desired location for signing. 
+                  *Mileage could apply if out of normal range</p>
+            </article>
+            <article>
+                <div>
+                  <h4>General Notary services</h4>
+                  <p>From $10.00</p>
+                </div>
+                <p>MVD Title/Bill of Sale, Wills, Trusts, Certified copies, POA's etc. Can perform in hospital, nursing home, jail, etc. Call for quote</p>
+            </article>
+            <article>
+                <div>
+                  <h4>Printing Services</h4>
+                  <p>From $0.25</p>
+                </div>
+                <p>Dual tray laser printer quality. Black& white copies only. Will deliver documents for 30 cents per mile
+                    After hours notarizations</p>
+            </article>
+            <article>
+                <div>
+                  <h4>After hours notarizations</h4>
+                </div>
+                <p>For last minute, after regular business hours signings. SEND MESSAGE FOR AVAILABLIITY. DO NOT CALL. After hours pricing will apply</p>
+            </article>
+          </section>
+        </section>
+        <section>
+          <h2>Testimonials</h2>
+          <article>
+            <h3>Tom McCarron</h3>
+            <p>6 days ago<br></br>Total Pro!!!<br></br>On time and Pleasure to work with.</p>
+          </article>
+          <article>
+            <h3>GLORIA Siqueiros</h3>
+            <p>very easy to work for you needs, and is more than professional, and very kind hearted.. she is a life savor.. amen will use her in the future if ever needed o and is very much responsible, very helpful thank you, <br></br>Gloria..</p>
+          </article>
+          <article>
+            <h3>Jeni eni</h3>
+            <p>Awesome, professional service! Met at my home for notary. Easy peasy and affordable 😁 will use again</p>
+          </article>
+        </section>
+        <section>
+          <h2>Contact</h2>
+        </section>
+      </main>
+      <footer>
+        Superstar Notary & Signing Services &copy; 2021
+      </footer>
     </div>
   );
 }
